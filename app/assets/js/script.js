f26c0569f653beb2c4c6c177547126ec7eafd277
Dominik Kukacka <dominik.kukacka@gmail.com>
2014-12-15 21:10:34 +0100

    angularized the whole thing

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,48 +1,65 @@
 // Responsive Navigation
 (function() {
-	var triggerBttn = document.getElementById( 'trigger-overlay' ),
-		overlay = document.querySelector( 'div.overlay' ),
-		closeBttn = overlay.querySelector( 'button.overlay-close' );
-		transEndEventNames = {
-			'WebkitTransition': 'webkitTransitionEnd',
-			'MozTransition': 'transitionend',
-			'OTransition': 'oTransitionEnd',
-			'msTransition': 'MSTransitionEnd',
-			'transition': 'transitionend'
-		},
-		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-		support = { transitions : Modernizr.csstransitions };
+    var triggerBttn = document.getElementById('trigger-overlay'),
+        overlay = document.querySelector('div.overlay'),
+        closeBttn = overlay.querySelector('button.overlay-close');
+    transEndEventNames = {
+            'WebkitTransition': 'webkitTransitionEnd',
+            'MozTransition': 'transitionend',
+            'OTransition': 'oTransitionEnd',
+            'msTransition': 'MSTransitionEnd',
+            'transition': 'transitionend'
+        },
+        transEndEventName = transEndEventNames[Modernizr.prefixed('transition')],
+        support = {
+            transitions: Modernizr.csstransitions
+        };
 
-	function toggleOverlay() {
-		if( classie.has( overlay, 'open' ) ) {
-			classie.remove( overlay, 'open' );
-			classie.add( overlay, 'close' );
-			var onEndTransitionFn = function( ev ) {
-				if( support.transitions ) {
-					if( ev.propertyName !== 'visibility' ) return;
-					this.removeEventListener( transEndEventName, onEndTransitionFn );
-				}
-				classie.remove( overlay, 'close' );
-			};
-			if( support.transitions ) {
-				overlay.addEventListener( transEndEventName, onEndTransitionFn );
-			}
-			else {
-				onEndTransitionFn();
-			}
-		}
-		else if( !classie.has( overlay, 'close' ) ) {
-			classie.add( overlay, 'open' );
-		}
-	}
+    function toggleOverlay() {
+        if (classie.has(overlay, 'open')) {
+            classie.remove(overlay, 'open');
+            classie.add(overlay, 'close');
+            var onEndTransitionFn = function(ev) {
+                if (support.transitions) {
+                    if (ev.propertyName !== 'visibility') return;
+                    this.removeEventListener(transEndEventName, onEndTransitionFn);
+                }
+                classie.remove(overlay, 'close');
+            };
+            if (support.transitions) {
+                overlay.addEventListener(transEndEventName, onEndTransitionFn);
+            } else {
+                onEndTransitionFn();
+            }
+        } else if (!classie.has(overlay, 'close')) {
+            classie.add(overlay, 'open');
+        }
+    }
 
-	triggerBttn.addEventListener( 'click', toggleOverlay );
-	closeBttn.addEventListener( 'click', toggleOverlay );
+    triggerBttn.addEventListener('click', toggleOverlay);
+    closeBttn.addEventListener('click', toggleOverlay);
 })();
 
-// Animation
-// $(document).ready(function() {
-// 	$('section.hero-home .content.container h1').addClass('animated fadeInDown');
-// 	$('section.hero-home .content.container p').addClass('animated fadeInDown');
-// 	$('section.hero-home .content.container .subscribe').addClass('animated fadeIn');
-// });
+var app = angular.module('appointio-marketing', ['ngRoute']);
+app.config(function($routeProvider) {
+    $routeProvider.
+        when('/index', {
+            templateUrl: 'default'
+        }).
+        when('/contact', {
+            templateUrl: 'contact'
+        }).
+        when('/price', {
+            templateUrl: 'price'
+        }).
+        when('/tos', {
+            templateUrl: 'tos'
+        }).
+        when('/team', {
+            templateUrl: 'team'
+        }).
+        when('/privacy', {
+            templateUrl: 'privacy'
+        }).
+        otherwise('/index');
+});
--- gruntfile.js
+++ gruntfile.js
@@ -7,20 +7,72 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         app: {
-            app: 'app'
+            app: 'app',
+            dist: 'dist'
         },
         express: {
             dev: {
                 options: {
                     port: 9000,
                     hostname: '0.0.0.0',
-                    bases: ['<%= app.app %>'],
+                    bases: [
+                        '<%= app.dist %>',
+                    ],
                     livereload: true
                 }
             }
-          }
+        },
+        watch: {
+            reload: {
+                files: [
+                    '<%= app.app %>/**/*'
+                ],
+                tasks: ['compile'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        copy: {
+            prod: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= app.app %>',
+                    src: [
+                        'assets/**/*',
+                        'index.html'
+                    ],
+                    dest: '<%= app.dist %>/'
+                }]
+            }
+        },
+        clean: {
+            prod: ['<%= app.dist %>']
+        },
+        ngtemplates: {
+            templates: {
+                src: ['<%= app.app %>/pages/**/*.html'],
+                dest: '<%= app.dist %>/assets/js/templates.js',
+                options: {
+                    bootstrap: function(module, script) {
+                        return "(function(angular) {\n\n" +
+                            "   var app = angular.module('appointio-marketing');" +
+                            "   app.run(['$templateCache', function($templateCache) {\n\n" + script + "\n\n}]);\n\n" +
+                            "}(window.angular));";
+                    },
+                    url: function(url) {
+                        var parts = url.match(/app\/pages\/(.*)\.html/);
+                        var name = parts[1];
+                        console.log('    - ' + name + ' packaged');
+                        return name;
+
+                    }
+                }
+            }
+        },
 
     });
 
-    grunt.registerTask('default', ['express', 'express-keepalive']);
+    grunt.registerTask('compile', ['clean', 'copy', 'ngtemplates']);
+    grunt.registerTask('default', ['compile', 'express', 'watch']);
 };
